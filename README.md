7f0e7c5f9b7236b8192d1933e1880e50b4a77cf4
Mathew Bray <mathew.bray@gmail.com>
2019-04-26 13:11:24 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# JavaScript-LaunchPad
+# LaunchPad JS
 
-Use nwjs
+This is an attempt.
+
+## Hold on to ya butts.
+
+To-Do:
+fix when search reveals one item it shows as a drop-down
+change everything so it uses the current working directory
---
+++ zzzzzz Send all to zip.ps1
@@ -0,0 +1,18 @@
+#--- Generate Current Date Variable$date = Get-Date
+$date = $date.ToString('yyyy-MM-dd_HH-mm-ss')    #--- App Path        $appFolder = "$($env:USERPROFILE)\Desktop\LaunchPad JS"
+
+$source = $appFolder
+$destination = "$env:TEMP\LaunchPad Backup $date.zip"
+$destinationFinal = "$appFolder\LaunchPad JS Backup $date.zip"
+
+If(Test-path $destination) {Remove-item $destination}
+
+Write-Host "Zipping..."
+
+[Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" )
+[System.IO.Compression.ZipFile]::CreateFromDirectory($Source, $destination) 
+
+Copy-Item $destination $destinationFinal
+
+$wshell = New-Object -ComObject Wscript.Shell
+$wshell.Popup("Backup Completed!",0,"Done!",0x1)
